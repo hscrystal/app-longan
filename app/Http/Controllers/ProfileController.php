35aaf93cb4ca6a9e3fe705dfd1f9f8afec6c4add
Unknown <hscrystaleng@gmail.com>
2018-07-25 15:07:54 +0700

    change function in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    function getName ()
+    function detail ()
     {
 	return "WebBookThai";
     }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/profile', 'ProfileController@getName');
+Route::get('/profile', 'ProfileController@detail');

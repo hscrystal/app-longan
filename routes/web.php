cd9f8ed35dca7fafbc6cf715891d686fb021c305
Unknown <hscrystaleng@gmail.com>
2018-07-25 15:35:50 +0700

    create route user

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user', function () {
+    return "Hscrystaleng";
+});
+
 Route::get('/profile', 'ProfileController@detail');
